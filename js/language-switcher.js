78274316dfe41e40182100c65a5db5e994bb8e2f
carat20g <carat.20g@gmail.com>
2023-06-04 20:24:08 +0900

    create common.json

--- js/language-switcher.js
+++ js/language-switcher.js
@@ -1,5 +1,30 @@
 let currentLanguage = 'en'; // Default language
 
+function loadCommon() {
+  // Construct the path to the common JSON file
+  const filePath = './json/common.json';
+
+  // Make an AJAX request to fetch the common JSON file
+  const xhr = new XMLHttpRequest();
+  xhr.overrideMimeType('application/json');
+  xhr.open('GET', filePath, true);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      const commonTranslations = JSON.parse(xhr.responseText);
+      // Update the common text content of elements using the commonTranslations object
+      updateCommonText(commonTranslations);
+    }
+  };
+  xhr.send();
+}
+
+
+function updateCommonText(commonTranslations) {
+  // Update the common text content of elements using the commonTranslations object
+  // Example: document.getElementById('elementId').textContent = commonTranslations.someText;
+}
+
+
 function loadLanguage(language) {
   // Construct the path to the JSON file based on the language
   const filePath = './json/${language}.json';
@@ -33,5 +58,9 @@ document.querySelectorAll('.topnav a').forEach((link) => {
   });
 });
 
+// Initialize the common texts on the website
+loadCommon();
+
 // Initialize the text on the website with the default language
 loadLanguage(currentLanguage);
+
