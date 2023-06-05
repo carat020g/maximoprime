ffd3b8ecdce4c21bf13e4425584990ab4e0e0d90
carat20g <carat.20g@gmail.com>
2023-06-05 17:13:58 +0900

    edit loadLanguage

--- js/language-switcher.js
+++ js/language-switcher.js
@@ -2,7 +2,7 @@ let currentLanguage = 'en'; // Default language
 let currentFontClass = 'font-sans-pro'; // Default font class
 
 function loadLanguage(language) {
-  const filePath = `/${language}.json`; // Update the file path based on the language
+  const filePath = `json/${language}.json`; // Update the file path based on the language
 
   fetch(filePath)
     .then(response => response.json())
