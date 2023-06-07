ee48c07296d42b20ad14b844b7cb6726849d3fda
carat20g <carat.20g@gmail.com>
2023-06-07 16:39:43 +0900

    test language-switcher.js

--- js/language-switcher.js
+++ js/language-switcher.js
@@ -1,32 +1,88 @@
-let currentLanguage = 'en'; // Default language
-let currentFontClass = 'font-sans-pro'; // Default font class
+// Language selection
+const langLinks = document.querySelectorAll('.topnav a');
 
-function loadLanguage(language) {
-  const filePath = `json/${language}.json`; // Update the file path based on the language
-
-  fetch(filePath)
-    .then(response => response.json())
-    .then(translations => {
-      updateText(translations);
-    })
-    .catch(error => {
-      console.error('Error fetching language JSON:', error);
-    });
-}
-
-function updateText(translations) {
-
-}
-
-// Add event listeners to the language selection links
-document.querySelectorAll('.topnav a').forEach(link => {
-  link.addEventListener('click', event => {
+langLinks.forEach(link => {
+  link.addEventListener('click', function(event) {
     event.preventDefault();
-    const selectedLanguage = link.id.slice(5); // Extract the language code from the link id
-    currentLanguage = selectedLanguage;
-    loadLanguage(currentLanguage);
+
+    // Remove active class from all links
+    langLinks.forEach(link => {
+      link.classList.remove('active');
+    });
+
+    // Set current link as active
+    this.classList.add('active');
+
+    // Get the language ID from the link's ID
+    const languageId = this.id.replace('lang_', '');
+
+    // Save the selected language option
+    saveLanguageOption(languageId);
+
+    // Change the website language
+    changeLanguage(languageId);
   });
 });
 
-// Initialize the text on the website with the default language
-loadLanguage(currentLanguage);
+// Function to save the selected language option
+function saveLanguageOption(languageId) {
+  // Use your preferred method (e.g., localStorage, cookies) to save the language option
+  // Example using localStorage:
+  localStorage.setItem('languageOption', languageId);
+}
+
+// Function to retrieve the saved language option
+function getSavedLanguageOption() {
+  // Use your preferred method to retrieve the saved language option
+  // Example using localStorage:
+  return localStorage.getItem('languageOption');
+}
+
+// Language change logic
+function changeLanguage(languageId) {
+  // Fetch language data using AJAX or fetch API
+  fetchLanguageData(languageId)
+    .then((data) => {
+      // Update the page content with the fetched language data
+      updatePageContent(data);
+    })
+    .catch((error) => {
+      console.error('Error fetching language data:', error);
+    });
+}
+
+// Function to fetch language data
+function fetchLanguageData(languageId) {
+  // Replace the URL with the actual endpoint to fetch language data
+  const url = `language-api-endpoint?lang=${languageId}`;
+
+  // Return a promise that resolves with the fetched language data
+  return fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Failed to fetch language data');
+      }
+      return response.json();
+    });
+}
+
+// Function to update the page content with the fetched language data
+function updatePageContent(languageData) {
+  // Update the main slogan
+  document.getElementById('slogan1').textContent = languageData.main.slogan.slogan1;
+  document.getElementById('slogan2').textContent = languageData.main.slogan.slogan2;
+
+  // Update the navigation menu
+  document.getElementById('nav_main').textContent = languageData.main.nav.main;
+  document.getElementById('nav_how_we_work').textContent = languageData.main.nav.howwework;
+  document.getElementById('nav_faq').textContent = languageData.main.nav.faq;
+  document.getElementById('nav_contact_us').textContent = languageData.main.nav.contact;
+}
+
+// Retrieve the saved language option and change the language on page load
+document.addEventListener('DOMContentLoaded', function() {
+  const savedLanguageOption = getSavedLanguageOption();
+  if (savedLanguageOption) {
+    changeLanguage(savedLanguageOption);
+  }
+});
--- js/nav.js
+++ js/nav.js
@@ -1,26 +1,50 @@
 
-// Handle menu item click events
-document.getElementById('nav_how_we_work').addEventListener('click', function() {
-    loadContent('how-we-work.html');
-  });
-  
-  // document.getElementById('mag').addEventListener('click', function() {
-  //   loadContent('products.html');
-  // });
-  
-  document.getElementById('nav_faq').addEventListener('click', function() {
-    loadContent('faq.html');
-  });
-  
-  // Function to load HTML content
-  function loadContent(htmlFile) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', htmlFile, true);
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        document.getElementById('content').innerHTML = xhr.responseText;
-      }
-    };
-    xhr.send();
+// Function to show content based on the clicked link
+function showContent(linkId) {
+  // Clear any existing content
+  document.getElementById('content').innerHTML = '';
+
+  // Implement the content change logic based on the link ID
+  switch (linkId) {
+    case 'nav_main':
+      // Code to show the main content
+      document.getElementById('content').innerHTML = '<h1>Main Content</h1>';
+      break;
+    case 'nav_how_we_work':
+      // Code to fetch and show the "How We Work" content
+      fetch('how-we-work.html')
+        .then(response => response.text())
+        .then(content => {
+          document.getElementById('content').innerHTML = content;
+        })
+        .catch(error => {
+          console.error('Error fetching "How We Work" content:', error);
+        });
+      break;
+    case 'nav_faq':
+      // Code to fetch and show the FAQ content
+      fetch('faq.html')
+        .then(response => response.text())
+        .then(content => {
+          document.getElementById('content').innerHTML = content;
+        })
+        .catch(error => {
+          console.error('Error fetching FAQ content:', error);
+        });
+      break;
+    case 'nav_contact_us':
+      // Code to fetch and show the Contact Us content
+      fetch('contact-us.html')
+        .then(response => response.text())
+        .then(content => {
+          document.getElementById('content').innerHTML = content;
+        })
+        .catch(error => {
+          console.error('Error fetching Contact Us content:', error);
+        });
+      break;
+    // Add more cases for other navigation links as needed
+    default:
+      break;
   }
-  
+}
